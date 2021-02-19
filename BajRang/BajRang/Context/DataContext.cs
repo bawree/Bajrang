5b5f25dc9fa694512968a0db1995882cf6240ec2
Ujjwal Gupta <i-Ujjwal.Gupta@icertis.com>
2021-02-19 23:17:48 +0530

    authentication

---
+++ BajRang/BajRang/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using BajRang.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BajRang.Context
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("ConStr")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BajRang/BajRang/Context/EventContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using BajRang.Models;
+
+namespace BajRang.Context
+{
+    public class EventContext:DbContext
+    {
+        public EventContext() : base("Constr")
+        {
+
+        }
+        public virtual DbSet<Event> Events { get; set; }
+    }
+}
