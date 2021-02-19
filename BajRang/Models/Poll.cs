397b89750e0f4ab08f974dc64fb8d805ce476f28
Akshay Kumar <i-Akshay.Kumar@icertis.com>
2021-02-19 15:08:28 +0530

    Poll class created.

---
+++ BajRang/Models/Poll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BajRang.Models
+{
+    public class Poll
+    {
+        public int PollId { get; set; }
+        public string Question { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
---
+++ BajRang/Models/PollOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BajRang.Models
+{
+    public class PollOption
+    {
+        public int PollOptionId { get; set; }
+        public int PollId { get; set; }
+        public string Answers { get; set; }
+        public int Vote { get; set; }
+
+        public virtual Poll Poll { get; set; }
+    }
+}
