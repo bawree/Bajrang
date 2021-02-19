eea5de5ac4d59b4180614ae01d67f684ecf490b9
Gaurav Singh <i-gaurav.singh@icertis.com>
2021-02-19 15:05:50 +0530

    Event Module Done

---
+++ BajRang/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BajRang.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public string Subject { get; set; }
+        public int Partcipant { get; set; }
+        public string Description { get; set; }
+        public System.DateTime Start { get; set; }
+        public Nullable<System.DateTime> End { get; set; }
+        public string ThemeColor { get; set; }
+    }
+}
