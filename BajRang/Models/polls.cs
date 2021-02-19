a21a495501942c03ac9a8b62540fca5c79541d51
Gaurav Singh <i-gaurav.singh@icertis.com>
2021-02-19 20:21:13 +0530

    Polls module done

---
+++ BajRang/Models/polls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BajRang.Models
+{
+    public class polls
+    {
+        
+            [Key]
+            public int PollId { get; set; }
+            public string Question { get; set; }
+        public bool Active { get; set; } = true;
+            public int Yes { get; set; } = 0;
+        public int No { get; set; } = 0;
+
+        public string Option1 { get; set; }
+        public string Option2 { get; set; }
+
+
+
+        
+    }
+}
